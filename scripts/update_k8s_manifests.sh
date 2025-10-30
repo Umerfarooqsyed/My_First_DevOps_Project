8739f9804d88665d6d7902baca5c85a5a2b45143
Umerfarooqsyed <umerfarooqsyed18@gmail.com>
2025-10-30 17:49:35 +0500

    Create update_k8s_manifests.sh

---
+++ scripts/update_k8s_manifests.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+
+#I have kept these variables here just for an idea. During the implementation these files will be kept in Azure key Vaults or any secure location.
+ACCESS_TOKEN="*******"
+ORGANIZATION_NAME=*******
+REGISTERY_NAME=*******
+IMAGE_REPOSITORY=*******
+AZURE_REPONAME=*******
+AZURE_PROJECTNAME=*******
+TAG=$BUILD_BUILDID 
+
+# Set the repository URL, I am not directly adding complete url for safety purpose. I have secured these tokens as variables in seperate secret file on my local.
+REPO_URL="https://$ACCESS_TOKEN@dev.azure.com/$ORGANIZATION_NAME/$AZURE_PROJECTNAME/_git/$AZURE_REPONAME"
+
+# Clone the git repository into the /tmp directory
+git clone "$REPO_URL" /tmp/temp_repo
+
+# Navigate into the cloned repository directory
+cd /tmp/temp_repo
+
+# Make changes to the Kubernetes manifest files
+# For example, let's say you want to change the image tag in a deployment.yaml file
+sed -i "s|image:.*|image: $REGISTERY_NAME/$IMAGE_REPOSITORY:$TAG|g" k8s/deployment.yaml
+
+# Add the modified files
+git add .
+
+# Commit the changes
+git commit -m "Update Kubernetes manifest"
+
+# Push the changes back to the repository
+git push
+
+# Cleanup: remove the temporary directory
+rm -rf /tmp/temp_repo
